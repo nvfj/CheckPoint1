ab79d9843767ec1e61c55ce9d5943b1a4a2d5e76
nvfj <ahlemmakhlouf88@gmail.com>
2024-09-08 21:20:32 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <nav class="nav">
+  <a class="nav-link active" href="#">Active</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#">Disabled</a>
+</nav>
+<h1> hello for new project </h1>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card text-white bg-secondary mb-3 card-custom">
+        <div class="card-body">
+          <h5 class="card-title">Secondary card title 1</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card text-white bg-secondary mb-3 card-custom">
+        <div class="card-body">
+          <h5 class="card-title">Secondary card title 2</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card text-white bg-secondary mb-3 card-custom">
+        <div class="card-body">
+          <h5 class="card-title">Secondary card title 3</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
